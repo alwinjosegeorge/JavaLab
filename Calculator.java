2c5035395fec3fcc8b4619b73356928750e0cbce
Alwin Jose George <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2025-07-21 14:33:10 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+
+class Calc{
+	public void add (int a ,int b ) {
+	int sum = a + b;
+	System.out.println("The sum of Two integer:" +sum);
+	}
+	
+	public void add (int a, int b , int c) {
+	int sum =a+b+c;
+	System.out.println("The sum of Three integer:" +sum);
+	}
+	
+	public void add (double a ,double b ) {
+	double sum =a+b;
+	System.out.println("The sum of Two Double:" +sum);
+	}
+	
+	
+	
+}
+public class Calculator {
+	public static void main(String[]args) {
+		Calc calc =new Calc();
+		calc.add(5, 5);
+		calc.add(2, 20,38);
+		calc.add(1.354876875637456876, 3421.74583296587451);
+	}
+	
+
+}
